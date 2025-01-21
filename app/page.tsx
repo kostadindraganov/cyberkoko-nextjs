f5476ea58b9ddb58c24db6fb3da8fea8e2855fb8
Kostadin Draganov <kostadindraganov@gmail.com>
2025-01-21 23:37:26 +0200

    init home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,90 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import NavBar from './components/Navbar'
+import FloatingImage from './components/Story'
+import { useLayoutEffect, useRef, useState } from "react"
+import gsap from "gsap"
+import Universe from './components/Universe'
+import WhoAreWe from './components/WhoAreWe'
+import Glance from './components/Glance'
+import Labels from './components/Labels'
+import Updates from './components/Updates'
+import Intro from './components/Intro'
+
+function App() {
+  const comp = useRef(null);
+  const [showContent, setShowContent] = useState(false);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      const t1 = gsap.timeline()
+
+      t1.from("#intro-slider", {
+        scale: 1,
+        duration: 1.5,
+        ease: "power3.out",
+        backgroundColor: "#000000",
+      })
+
+        .to("#intro-slider", {
+          // scale: 1, 
+          duration: 1.3,
+          ease: "power3.inOut",
+          backgroundColor: "#5729ff",
+        })
+        .from(["#title-1", "#title-2", "#title-3", "#title-4"], {
+          // opacity: 0,
+          y: "-100vh",
+          stagger: 0.6,
+          ease: "circ.inOut",
+        }).to(["#title-1", "#title-2", "#title-3", "#title-4"], {
+          opacity: 0,
+          y: "-100vh",
+          ease: "back.out",
+          stagger: 0.5,
+        })
+        .to("#intro-slider", {
+          scale: 0,
+          // borderRadius: "100%",
+          backgroundColor: "black",
+          duration: 0.5,
+          onComplete: () => setShowContent(true)
+        })
+
+    }, comp)
+
+    return () => ctx.revert()
+  }, [])
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <main ref={comp} className='relative min-h-screen w-screen overflow-x-hidden '>
+     <Intro />
+      {
+        showContent && (
+          <>
+            <NavBar />
+            <Hero />
+            <About />
+            <Features />
+            <FloatingImage />
+            <Universe />
+            <WhoAreWe />
+            <Glance />
+            <Labels />
+            <Updates />
+            <Contact />
+            <Footer />
+          </>
+        )
+      }
+    </main >
+  )
 }
+
+export default App;
